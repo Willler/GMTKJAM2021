6085e6d79aaa6f6ed59cacd789d51e2f7db98280
William L'Eriger <espermentalout@gmail.com>
2021-06-11 22:14:12 -0400

    Let's try to fix this

--- PT/Prototype/Assets/scripts/MenuScript.cs
+++ PT/Prototype/Assets/scripts/MenuScript.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuScript : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public Text startMenuText;
+
     void Start()
     {
         
+    }
+
+    public void onPointerEnter()
+    {
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+   public void nextSceneButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
